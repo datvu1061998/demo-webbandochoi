a3fe5ccf36ab730608a2e10a765ec5f70c5cd249
Vũ Tấn Đạt <33850332+datvu1061998@users.noreply.github.com>
2019-05-27 00:43:05 +0700

    them-form

--- js/trang-mua-hang.js
+++ js/trang-mua-hang.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
-    $(".nav-content .nav-link").click(function (e) { 
+    $(".nav-content .nav-link").click(function (e) {
         e.preventDefault();
         $(".nav-content .nav-link").removeClass("current");
         $(this).addClass("current");
+    });
+    var lengthul = $(".ul-list-hinhNho li").length;
+    lengthul -= 3;
+
+    var click_right = 0;
+    var click_left = lengthul;
+    $(".right").click(function (e) {
+        if (lengthul > click_right) {
+            click_right++;
+            click_left--;      
+            var leftul = parseInt($(".ul-list-hinhNho").css("left"));
+            leftul -= 80;
+            e.preventDefault();
+            TweenLite.to($(".ul-list-hinhNho"), 0.4, { left: leftul + "px", ease: Power3.easeOut });
+        }
+    });
+    $(".left").click(function (e) {
+        if (lengthul > click_left) {
+            click_left++;
+            click_right--;
+            var leftul = parseInt($(".ul-list-hinhNho").css("left"));
+            leftul += 80;
+            e.preventDefault();
+            TweenLite.to($(".ul-list-hinhNho"), 0.4, { left: leftul + "px", ease: Power3.easeOut });
+        }
     });
 });
